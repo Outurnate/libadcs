b769419a72df499f31dfb36b1521bad5fe9e7da5
Outurnate <joseph@outurnate.com>
2023-03-12 17:40:15 -0400

    Slightly less broken schemas

--- src/schemas.rs
+++ src/schemas.rs
@@ -8,14 +8,14 @@ struct WSSecurityExtensions;
 
 #[derive(Debug, XmlSchema)]
 #[xml_schema(source = "src/wstep.xsd", target_prefix = "wstep",
-  module_namespace_mapping = "crate::schemas::w_step::types"
+  module_namespace_mapping = "crate::schemas::ws_tep::types"
 )]
-struct WStep;
+struct WSTep;
 
 #[derive(Debug, XmlSchema)]
 #[xml_schema(source = "src/wst.xsd", target_prefix = "wst",
   module_namespace_mapping = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd: crate::schemas::ws_security_extensions::types",
-  module_namespace_mapping = "http://schemas.microsoft.com/windows/pki/2009/01/enrollment: crate::schemas::w_step::types",
+  module_namespace_mapping = "http://schemas.microsoft.com/windows/pki/2009/01/enrollment: crate::schemas::ws_tep::types",
   module_namespace_mapping = "crate::schemas::ws_trust::types"
 )]
 struct WSTrust;
@@ -33,22 +33,38 @@ struct XCEP;
 struct WSA;
 
 #[derive(Debug, XmlSchema)]
-#[xml_schema(source = "src/soap.xsd", target_prefix = "soap",
+#[xml_schema(source = "src/soap-core.xsd", target_prefix = "soap",
+  module_namespace_mapping = "http://www.w3.org/2005/08/addressing: crate::schemas::wsa::types"
+)]
+struct SOAPCore;
+
+#[derive(Debug, XmlSchema)]
+#[xml_schema(source = "src/soap-xcep.xsd", target_prefix = "soap",
   module_namespace_mapping = "http://schemas.microsoft.com/windows/pki/2009/01/enrollmentpolicy: crate::schemas::xcep::types",
+  module_namespace_mapping = "http://www.w3.org/2005/08/addressing: crate::schemas::wsa::types"
+)]
+struct SOAPXcep;
+
+#[derive(Debug, XmlSchema)]
+#[xml_schema(source = "src/soap-wst.xsd", target_prefix = "soap",
   module_namespace_mapping = "http://docs.oasis-open.org/ws-sx/ws-trust/200512/: crate::schemas::ws_trust::types",
   module_namespace_mapping = "http://www.w3.org/2005/08/addressing: crate::schemas::wsa::types"
 )]
-struct SOAP;
+struct SOAPWst;
 
 pub(crate) use ws_security_extensions::types::*;
 pub(crate) use ws_security_extensions::*;
-pub(crate) use w_step::types::*;
-pub(crate) use w_step::*;
+pub(crate) use ws_tep::types::*;
+pub(crate) use ws_tep::*;
 pub(crate) use ws_trust::types::*;
 pub(crate) use ws_trust::*;
 pub(crate) use xcep::types::*;
 pub(crate) use xcep::*;
 pub(crate) use wsa::types::*;
 pub(crate) use wsa::*;
-pub(crate) use soap::types::*;
-pub(crate) use soap::*;
+pub(crate) use soap_core::types::*;
+pub(crate) use soap_core::*;
+pub(crate) use soap_xcep::types::*;
+pub(crate) use soap_xcep::*;
+pub(crate) use soap_wst::types::*;
+pub(crate) use soap_wst::*;
